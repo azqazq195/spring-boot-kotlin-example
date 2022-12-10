303fd5eed6793296c2458dd554210978d5ee9e9b
seongha.moon <azqazq195@gmail.com>
2022-12-10 20:38:21 +0900

    refactor: ExceptionHandler 상속받지 않고 Custom 으로 작성

--- src/main/kotlin/com/example/springbootkotlinexample/common/advice/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/advice/GlobalExceptionHandler.kt
@@ -4,14 +4,28 @@ import com.example.springbootkotlinexample.common.dto.ResponseDto
 import com.example.springbootkotlinexample.common.exception.NotFoundException
 import com.example.springbootkotlinexample.config.logger
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 
 @RestControllerAdvice
-class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
+class GlobalExceptionHandler {
     private val log = logger()
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ResponseDto<Void> {
+        log.warn("http message not readable exception {}", e.message)
+        return ResponseDto(HttpStatus.BAD_REQUEST, e.message)
+    }
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseDto<Void> {
+        val firstFieldError = e.bindingResult.allErrors[0] as FieldError
+        val message = "${firstFieldError.field}: ${firstFieldError.defaultMessage}"
+        return ResponseDto(HttpStatus.BAD_REQUEST, message)
+    }
 
     @ExceptionHandler(NotFoundException::class)
     fun handleApiException(e: NotFoundException): ResponseDto<Void> {
@@ -20,7 +34,7 @@ class GlobalExceptionHandler : ResponseEntityExceptionHandler() {
     }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(e: Exception?): ResponseDto<Void> {
+    fun unHandleException(e: Exception?): ResponseDto<Void> {
         log.error("unhandled exception {}", e?.message)
         return ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR)
     }
