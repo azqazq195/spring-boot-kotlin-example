8d8b0a66477db254f27a14eeb0f56622cdae24a5
seongha.moon <azqazq195@gmail.com>
2022-12-11 13:51:49 +0900

    refactor: ResponseDto constructor 정리

--- src/main/kotlin/com/example/springbootkotlinexample/common/dto/ResponseDto.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/dto/ResponseDto.kt
@@ -5,28 +5,52 @@ import org.springframework.http.ResponseEntity
 import java.util.Date
 
 class ResponseDto<T> : ResponseEntity<ResponseDto.Response<T>> {
-    constructor(httpStatus: HttpStatus) : super(Response(httpStatus), httpStatus)
-    constructor(httpStatus: HttpStatus, data: T) : super(Response(httpStatus, data), httpStatus)
-    constructor(httpStatus: HttpStatus, message: String?) : super(Response(httpStatus, message), httpStatus)
+    constructor(httpStatus: HttpStatus) : super(
+        Response(httpStatus),
+        httpStatus
+    )
+
+    constructor(httpStatus: HttpStatus, data: T) : super(
+        Response(httpStatus, data),
+        httpStatus
+    )
+
+    constructor(httpStatus: HttpStatus, message: String?) : super(
+        if (message == null) Response(httpStatus) else Response(httpStatus, message),
+        httpStatus
+    )
+
     constructor(httpStatus: HttpStatus, message: String?, data: T) : super(
-        Response(httpStatus, message ?: httpStatus.reasonPhrase, data),
+        if (message == null) Response(httpStatus, data) else Response(httpStatus, message, data),
         httpStatus
     )
 
     data class Response<T>(val timestamp: Date, val status: Int, val message: String, val data: T?) {
-        constructor(httpStatus: HttpStatus) : this(Date(), httpStatus.value(), httpStatus.reasonPhrase, null)
-        constructor(httpStatus: HttpStatus, data: T?) : this(Date(), httpStatus.value(), httpStatus.reasonPhrase, data)
-        constructor(httpStatus: HttpStatus, message: String?) : this(
+        constructor(httpStatus: HttpStatus) : this(
             Date(),
             httpStatus.value(),
-            message ?: httpStatus.reasonPhrase,
+            httpStatus.reasonPhrase,
             null
         )
 
-        constructor(httpStatus: HttpStatus, message: String?, data: T) : this(
+        constructor(httpStatus: HttpStatus, message: String) : this(
             Date(),
             httpStatus.value(),
-            message ?: httpStatus.reasonPhrase,
+            message,
+            null
+        )
+
+        constructor(httpStatus: HttpStatus, data: T) : this(
+            Date(),
+            httpStatus.value(),
+            httpStatus.reasonPhrase,
+            data
+        )
+
+        constructor(httpStatus: HttpStatus, message: String, data: T) : this(
+            Date(),
+            httpStatus.value(),
+            message,
             data
         )
     }
