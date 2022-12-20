9c81064e796dd12ac9c017b579eb049df2aabc6e
moseoh <azqazq195@gmail.com>
2022-12-20 14:49:49 +0900

    Feat relation object (#20)
    * refactor: javadoc 수정
    
    * refactor: product domain 삭제

--- src/main/kotlin/com/example/springbootkotlinexample/domain/example/controller/dto/CreateExampleDto.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/domain/example/controller/dto/CreateExampleDto.kt
@@ -8,10 +8,19 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 /**
- * field에 null이 허용되는 이유
+ * CreateExampleDto
+ *
+ * * field에 null이 허용되는 이유
  * * null이 허용되지 않는 경우 constuctor에서 null을 받을 수 없음
- * * 생성자체가 되지 않아 validation 에러를 발생시킬 수 없다.
+ * * 생성자체가 되지 않기 때문에 validation 에러를 발생시킬 수 없다.
  * * @NotNull annotation을 활용하여 검사한다.
+ *
+ * @property name 이름명: null 허용하지 않음, 최소 2자, 최대 5자
+ * @property content 설명: null 허용하지 않음, 최소 2자, 최대 100자
+ * @property count 개수: null 허용하지 않음, 최대 100
+ * @property enum enum: null 허용하지 않음
+ * @see ExampleEnum
+ *
  */
 class CreateExampleDto(
     @field:NotNull
