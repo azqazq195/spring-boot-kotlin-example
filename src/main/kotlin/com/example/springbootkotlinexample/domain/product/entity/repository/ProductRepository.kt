65edcb0738c4291a17fa6beb26ce97e46f2c831c
seongha.moon <azqazq195@gmail.com>
2022-12-20 13:51:50 +0900

    refactor: product domain 삭제

--- src/main/kotlin/com/example/springbootkotlinexample/domain/product/entity/repository/ProductRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.springbootkotlinexample.domain.product.entity.repository
-
-import com.example.springbootkotlinexample.common.base.entity.repository.IRepository
-import com.example.springbootkotlinexample.domain.product.entity.Product
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ProductRepository : IRepository<Product>
