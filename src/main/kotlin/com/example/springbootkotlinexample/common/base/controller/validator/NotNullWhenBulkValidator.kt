2f5e4b9b0f149234522c30a8f78aed1f9d5495c1
seongha.moon <azqazq195@gmail.com>
2022-12-22 17:50:15 +0900

    refactor: 폴더 정리

--- src/main/kotlin/com/example/springbootkotlinexample/common/base/controller/validator/NotNullWhenBulkValidator.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/base/controller/validator/NotNullWhenBulkValidator.kt
@@ -1,4 +1,4 @@
-package com.example.springbootkotlinexample.common.base.validator
+package com.example.springbootkotlinexample.common.base.controller.validator
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.ConstraintValidator
--- src/main/kotlin/com/example/springbootkotlinexample/domain/user/user/service/exception/NotFoundUserException.kt
+++
@@ -1,5 +0,0 @@
-package com.example.springbootkotlinexample.domain.user.user.service.exception
-
-import com.example.springbootkotlinexample.common.base.service.exception.NotFoundEntityException
-
-class NotFoundUserException : NotFoundEntityException(entityName = "사용자")
