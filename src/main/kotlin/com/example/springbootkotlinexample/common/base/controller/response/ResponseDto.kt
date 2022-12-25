244406d2987b1b5c453a4e80327c7a0485397a04
moseoh <azqazq195@gmail.com>
2022-12-25 18:30:11 +0900

    refactor: Not Supported Exception 추가 및 적용 (#34)

--- src/main/kotlin/com/example/springbootkotlinexample/common/base/controller/response/ResponseDto.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/base/controller/response/ResponseDto.kt
@@ -1,4 +1,4 @@
-package com.example.springbootkotlinexample.common.base.response
+package com.example.springbootkotlinexample.common.base.controller.response
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
