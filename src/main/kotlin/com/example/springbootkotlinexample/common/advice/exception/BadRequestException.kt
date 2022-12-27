807308c6fcdac6ccdade74783aff65a442584a8d
moseoh <azqazq195@gmail.com>
2022-12-27 14:51:48 +0900

    Refactor exception (#38)
    * refactor: Exception 명칭 정리 및 status code 정리
    
    * refactor: 문법 정리
    
    * feat: NotFoundException 추가

--- src/main/kotlin/com/example/springbootkotlinexample/common/advice/exception/BadRequestException.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/advice/exception/BadRequestException.kt
@@ -1,3 +1,3 @@
 package com.example.springbootkotlinexample.common.advice.exception
 
-open class BadRequestException(message: String?) : RuntimeException(message ?: "잘못된 요청입니다.")
+open class BadRequestException(message: String? = null) : RuntimeException(message ?: "잘못된 요청입니다.")
--- src/main/kotlin/com/example/springbootkotlinexample/common/base/service/exception/RequiredArgumentException.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/common/base/service/exception/RequiredArgumentException.kt
@@ -2,4 +2,4 @@ package com.example.springbootkotlinexample.common.base.service.exception
 
 import com.example.springbootkotlinexample.common.advice.exception.BadRequestException
 
-class RequiredArgumentException(argName: String) : BadRequestException("$argName 값은 필수 값 입니다.")
+open class RequiredArgumentException(argName: String) : BadRequestException("$argName 값은 필수 값 입니다.")
