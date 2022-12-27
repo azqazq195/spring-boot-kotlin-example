0fc0ea814bb862c8c8ff161d6513e480e6af03dd
seongha.moon <azqazq195@gmail.com>
2022-12-27 14:18:19 +0900

    feat: Base ExistsEntity Validator 추가

---
+++ src/main/kotlin/com/example/springbootkotlinexample/common/base/controller/dto/validator/ExistsEntity.kt
@@ -0,0 +1,16 @@
+package com.example.springbootkotlinexample.common.base.controller.dto.validator
+
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@Constraint(validatedBy = [ExistsEntityValidator::class])
+annotation class ExistsEntity(
+    val entity: KClass<*>,
+
+    val message: String = "존재하지 않는 엔티티입니다.",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
