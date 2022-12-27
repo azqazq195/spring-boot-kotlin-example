54b8642eeb9cb30a708466f1845fbf923527ee15
moseoh <azqazq195@gmail.com>
2022-12-27 14:20:50 +0900

    Feat base entity validator (#37)
    * feat: Base ExistsEntity Validator 추가
    
    * feat: ExistsEntity Validator 적용
    
    * refactor: Service 대신 EntityManager를 사용하도록 변경

--- src/main/kotlin/com/example/springbootkotlinexample/domain/user/controller/validator/NotExistsUsernameValidator.kt
+++ src/main/kotlin/com/example/springbootkotlinexample/domain/user/controller/validator/NotExistsUsernameValidator.kt
@@ -1,13 +1,16 @@
 package com.example.springbootkotlinexample.domain.user.controller.validator
 
-import com.example.springbootkotlinexample.domain.user.service.UserService
+import com.example.springbootkotlinexample.domain.user.entity.User
+import jakarta.persistence.EntityManager
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 
 class NotExistsUsernameValidator(
-    private val userService: UserService,
+    private val entityManager: EntityManager,
 ) : ConstraintValidator<NotExistsUsername, String> {
     override fun isValid(value: String, context: ConstraintValidatorContext?): Boolean {
-        return !userService.isExistsByUsername(value)
+        val typedQuery = entityManager.createQuery("SELECT u FROM tb_user u WHERE u.username = :username", User::class.java)
+        typedQuery.setParameter("username", value)
+        return typedQuery.resultList.size == 0
     }
 }
