a5d6882abb719d0792c1bdba594eb519d14ad12e
moseoh <azqazq195@gmail.com>
2023-04-25 05:53:47 +0900

    Refactor(script): docker 실행 스크립트 로딩 추가

--- jwt/docker-up.sh
+++ jwt/docker-up.sh
@@ -1,7 +1,18 @@
-#bin/bash
+#!/bin/bash
+# shellcheck disable=SC1090
+
+source ~/Documents/code/script/loading-animation.sh
+source ~/Documents/code/script/color.sh
 
 # Docker App 실행
 open -a Docker
+
+loading_animation_start "Waiting for Docker to start"
+while ! docker info &> /dev/null ; do
+  sleep 1
+done
+loading_animation_stop
+echo "${GREEN}done.${NC}"
 
 # -f docker-compose 파일 위치 설정
 # -p Project Name 설정
