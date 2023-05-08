ac3c61769fe2e33135bfb9785580f5681d486c9d
moseoh <azqazq195@gmail.com>
2023-05-08 14:28:01 +0900

    rename: 사용하지 않는 class 제거

--- jwt/src/main/kotlin/com/example/jwt/user/domain/UserDetailsImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jwt.user.domain
-
-data class UserDetailsImpl(
-    val user: User
-) : org.springframework.security.core.userdetails.User(
-    user.email,
-    user.password,
-    emptyList()
-)
