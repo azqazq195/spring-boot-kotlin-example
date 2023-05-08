2918dd6b83f441b86c97c9fada2d386c5a981a4f
moseoh <azqazq195@gmail.com>
2023-05-08 13:23:17 +0900

    refactor: kotlin 버전 변경

--- jwt/build.gradle.kts
+++ jwt/build.gradle.kts
@@ -3,17 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.20"
     /**
      * kotlin-spring 은 all-open 위에 래핑됩니다.
      */
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("plugin.spring") version "1.8.20"
     /**
      * kotlin-jpa 는 no-arg 위에 래핑됩니다.
      * 플러그인은 @Entity, @Embeddable 및 @MappedSuperclass no-arg 주석을 자동으로 지정합니다.
      * @link <a href="https://kotlinlang.org/docs/no-arg-plugin.html#jpa-support">JPA Support</a>
      */
-    kotlin("plugin.jpa") version "1.7.22"
+    kotlin("plugin.jpa") version "1.8.20"
 }
 
 group = "com.example"
@@ -48,6 +48,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.13.4")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile> {
