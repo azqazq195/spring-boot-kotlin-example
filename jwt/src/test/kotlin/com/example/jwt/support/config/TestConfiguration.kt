617312060c9bc8e0275a10ceaa2bba27c37350ec
moseoh <azqazq195@gmail.com>
2023-05-11 23:34:47 +0900

    rename: 파일 이동

--- jwt/src/test/kotlin/com/example/jwt/support/config/TestConfiguration.kt
+++ jwt/src/test/kotlin/com/example/jwt/support/config/TestConfiguration.kt
@@ -1,4 +1,4 @@
-package com.example.jwt.support
+package com.example.jwt.support.config
 
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
